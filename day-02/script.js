ee14a6806e72c5b9f5d5c6a9171cea0e54551c2a
raviteja-nagisetti <raviteja.nagisetti28@gmail.com>
2025-06-18 14:59:13 +0530

    Day2 Tasks

---
+++ day-02/script.js
@@ -0,0 +1,18 @@
+let name = "Ravi Teja Nagisetti";
+let age = 28;
+let isStudent = false;
+let favoriteProgrammingLang = "JavaScript";
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(favoriteProgrammingLang);
+
+let person = {
+  name: "Ravi",
+  age: 28,
+  isStudent: false,
+  favoriteProgrammingLang: "JavaScript",
+};
+
+let arr = [2, "Hello", true, null, undefined];
